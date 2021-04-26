84c5099385a727ffe3f58c0524114ae402af74af
b19karhj <b19karhj@student.his.se>
2021-04-26 11:58:44 +0200

    Lagt till jason koden

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
             public void onItemClick (AdapterView<?> parent, View V, int poistion, long id) {
-                Toast.makeText(getApplicationContext(), "Enter your text here", Toast.LENGTH_SHORT).show(); //Toast skriver ut text.
+                Toast.makeText(getApplicationContext(), mountainArrayList.get(poistion).info(),Toast.LENGTH_SHORT).show(); //Skriver ut värdet i mountainArraylist.
 
             }
         });
@@ -35,5 +35,50 @@ public class MainActivity extends AppCompatActivity {
         mountainArrayList.add(new Mountain("Mont Blanc","Alps",4808));
         mountainArrayList.add(new Mountain("Denali","Alaska",6190));
 
+    }
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+        }
     }
 }
