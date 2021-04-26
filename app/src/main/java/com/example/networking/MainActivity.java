037f1b6b4d455c07874ab9f1ad6f2facc7d6b511
b19karhj <b19karhj@student.his.se>
2021-04-26 11:41:00 +0200

    Added Adapter and added my list_v

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,18 +3,26 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public ArrayList<Mountain> mountainArraylist=new ArrayList(); //
+    public ArrayList<Mountain> mountainArrayList=new ArrayList(); //Skapar en ny lista med namnet mountainArrayList. Värden av typen mountain som är skapad.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<Mountain> adapter=new ArrayAdapter<Mountain>(this, R.layout.list_item_textview,R.id.text_T,mountainArrayList); //Adapter pekar på layouten och sedan på id:et och tar in värdet av mountainArrayList. Adapter kopplar samman alla.
+        ListView my_listview=(ListView) findViewById(R.id.list_V);
 
+
+        mountainArrayList.add(new Mountain("Matterhorn","Alps",4478));
+        mountainArrayList.add(new Mountain("Mont Blanc","Alps",4808));
+        mountainArrayList.add(new Mountain("Denali","Alaska",6190));
 
     }
 }
