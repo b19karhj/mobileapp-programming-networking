2dda466db9a340a9ddc5406e97456bb005eae98c
b19karhj <b19karhj@student.his.se>
2021-04-26 11:04:00 +0200

    Added class mountain and created strings

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,16 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private String location;
+    private int height;
+
+    //constructor
+    public Mountain(String n,String l,int h)
+    {
+        name=n;
+        location=l;
+        height=-1;
+
+
+}
